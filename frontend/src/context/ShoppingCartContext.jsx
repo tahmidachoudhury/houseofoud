0de18f0ead7ea5ac6b5a95596f1a1cd4db144cb2
Tahmid Choudhury <tahmidachoudhury@outlook.com>
2024-06-05 15:45:22 +0100

    add cart functionality.
    get item quantity, increase and decrease item
    quantity.

--- frontend/src/context/ShoppingCartContext.jsx
+++ frontend/src/context/ShoppingCartContext.jsx
@@ -9,12 +9,46 @@ export function useShoppingCart() {
 export function ShoppingCartProvider({ children }) {
   const [cartItems, setCartItems] = useState([]);
 
-  function getItemQuantity() {
+  function getItemQuantity(id) {
     return cartItems.find((item) => item.id === id)?.quantity || 0;
   }
 
+  function increaseCartQuantity(id) {
+    setCartItems((currItems) => {
+      if (currItems.find((item) => item.id === id) == null) {
+        return [...currItems, { id, quantity: 1 }];
+      } else {
+        return currItems.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity + 1 };
+          } else {
+            return item;
+          }
+        });
+      }
+    });
+  }
+
+  function decreaseCartQuantity(id) {
+    setCartItems((currItems) => {
+      if (currItems.find((item) => item.id === id)?.quantity === 1) {
+        return currItems.filter((item) => item.id !== id);
+      } else {
+        return currItems.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity - 1 };
+          } else {
+            return item;
+          }
+        });
+      }
+    });
+  }
+
   return (
-    <ShoppingCartContext.Provider value={{ getItemQuantity }}>
+    <ShoppingCartContext.Provider
+      value={{ getItemQuantity, increaseCartQuantity, decreaseCartQuantity }}
+    >
       {children}
     </ShoppingCartContext.Provider>
   );
