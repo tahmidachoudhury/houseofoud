abc456df1b682835323799d139919206517cfa3e
Tahmid Choudhury <tahmidachoudhury@outlook.com>
2024-06-27 22:20:44 +0100

    urls.py configuration

--- backend/houseofoud/api/urls.py
+++ backend/houseofoud/api/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('reviews', views.getReviews),
-    path('addreviews/', views.addReview),
-    path('users', views.getUser),
-    path('addusers/', views.addUser),
-    path('products/', views.getProducts)
+    path('reviews/', views.getReviews, name='get_reviews'),
+    path('addreviews/', views.addReview, name='add_review'),
+    path('users/', views.getUser, name='get_user'),
+    path('addusers/', views.addUser, name='add_user'),
+    path('products/', views.getProducts, name='get-products')
 ]
