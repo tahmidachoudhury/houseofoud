0cf6afcda015469fea843f2c5792a2c4f874853b
Tahmid Choudhury <tahmidachoudhury@outlook.com>
2024-08-19 12:59:34 +0100

    refactoring shop page

--- frontend/src/components/indexPage/BestSellerSection/Product.jsx
+++ frontend/src/components/indexPage/BestSellerSection/Product.jsx
@@ -18,42 +18,37 @@ function Product(props) {
     aspectRatio: "3/4",
   }
 
-  const productInfo = {
-    padding: "15px 0 ",
-  }
-
   return (
     <Box position="relative" zIndex={1}>
-      <Link to={`/product/${props.link}`} key={props.link}>
-        <Box
-          width="100%"
-          height="100%"
-          display="flex"
-          flexDirection="column"
-          position="relative"
-          zIndex={0}
-          sx={{ pb: 5 }}
-        >
+      <Box
+        width="100%"
+        height="100%"
+        display="flex"
+        flexDirection="column"
+        position="relative"
+        zIndex={0}
+      >
+        <Link to={`/product/${props.link}`} key={props.link}>
           <div className="image-container" style={{ ...cntrStyle }} />
+        </Link>
 
-          <div style={productInfo}>
-            <ThemeProvider theme={themeOptions}>
-              <Typography gutterBottom>{props.type}</Typography>
-              <Typography variant="body2" gutterBottom>
-                {props.name}
-              </Typography>
-              <Grid container>
-                <Grid item xs={6}>
-                  <Typography gutterBottom>{props.price}</Typography>
-                </Grid>
-                <Grid item xs={6} display="flex" justifyContent="right">
-                  <Typography gutterBottom>{props.size}</Typography>
-                </Grid>
+        <Box py="15px">
+          <ThemeProvider theme={themeOptions}>
+            <Typography gutterBottom>{props.type}</Typography>
+            <Typography variant="body2" gutterBottom>
+              {props.name}
+            </Typography>
+            <Grid container>
+              <Grid item xs={6}>
+                <Typography gutterBottom>{props.price}</Typography>
               </Grid>
-            </ThemeProvider>
-          </div>
+              <Grid item xs={6} display="flex" justifyContent="right">
+                <Typography gutterBottom>{props.size}</Typography>
+              </Grid>
+            </Grid>
+          </ThemeProvider>
         </Box>
-      </Link>
+      </Box>
     </Box>
   )
 }
