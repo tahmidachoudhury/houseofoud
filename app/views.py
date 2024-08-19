449cf05b735db097d634f03a5c5e0f95757d4d18
Tahmid Choudhury <tahmidachoudhury@outlook.com>
2024-08-19 15:46:00 +0100

    line item fix

--- app/views.py
+++ app/views.py
@@ -31,7 +31,7 @@ else:
 #             'quantity': item['quantity'],
 #         })
 #     return line_items
-<<<<<<< HEAD
+
 
 # production---------------------------------------------------------------------------
 
@@ -51,12 +51,10 @@ def append_cart_to_line_items(cart):
         except Product.DoesNotExist:
             print(f"Product with id {item['id']} does not exist.")
         except Price.DoesNotExist:
-            print(f"Price for product {item['id']} and size {
-                  item['size']} does not exist.")
+            print(f"Price for product {item['id']} and size {item['size']} does not exist.")
 
     return line_items
-=======
->>>>>>> 0cf6afcda015469fea843f2c5792a2c4f874853b
+
 
 # production---------------------------------------------------------------------------
 
